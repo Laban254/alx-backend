fe66eafc5a7bd9eb7638430dab8b32a2473d30d6
laban254 <labanrotich6544@gmail.com>
2023-08-01 22:25:16 +0300

    fixed pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """Basic Flask app """
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def home():
+def index():
+    """Function documentation: Renders the index.html template."""
     return render_template("index.html")
 
 
