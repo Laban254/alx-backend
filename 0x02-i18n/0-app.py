753fa54e20d5e0d1e678a884fe7cb8e44c70d3ae
laban254 <labanrotich6544@gmail.com>
2023-08-03 13:11:20 +0300

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,11 +6,11 @@ from flask import Flask, render_template
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route("/")
+@app.route('/')
 def index():
     """Function documentation: Renders the index.html template."""
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
